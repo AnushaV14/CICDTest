480056b96744134c6d8c6d61ff535e36caf3562e
Anusha Vujjini <anusha_vujjini@epam.com>
2024-06-17 17:13:24 +0530

    modified jenkins file

--- Jenkinsfile
+++ Jenkinsfile
@@ -30,12 +30,12 @@ pipeline {
 
     stage('archive') {
       steps {	
-        archiveArtifacts artifacts: 'target/*.jar'	
+        archiveArtifacts artifacts: 'test-output/*.war'	
       }
     }
     stage('deploy') {
 	steps{
-	   deploy adapters: [tomcat9(credentialsId: '2b80f3f4-6843-4733-9807-47b3d11d8964', path: '', url: 'http://localhost:9999/')], contextPath: 'test', onFailure: false, war: 'target/*.jar'
+	   deploy adapters: [tomcat9(credentialsId: '2b80f3f4-6843-4733-9807-47b3d11d8964', path: '', url: 'http://localhost:9999/')], contextPath: 'test', onFailure: false, war: 'test-output/*.war'
 	}
     }
 
--- src/test/java/com/epam/test/TestOne.java
+++ src/test/java/com/epam/test/TestOne.java
@@ -1,5 +1,6 @@
 package com.epam.test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.epam.utilities.TestUtility;
@@ -11,4 +12,30 @@ public class TestOne {
 	public void testM() {
 		System.out.println("test successful :"+	test.generateRandomNumber());
 	}
+	
+
+	@Test
+	public void testSubtract() {
+		long a = 25;
+		long b = 20;
+		long expectedResult = 5;
+		// long result = calculatorTest.sub(a, b);
+		long result = a - b;
+		//_test.info("Input values are :" + a + " , " + b);
+		Assert.assertEquals(expectedResult, result);
+		//_test.pass("Subtraction is working as expected , result is :" + result);
+	}
+
+	@Test
+	public void testMultiply() {
+		long a = 10;
+		long b = 25;
+		long expectedResult = 250;
+		long result = a * b;
+		// long result = calculatorTest.mult(a, b);
+		//_test.info("Input values are :" + a + " , " + b);
+		Assert.assertEquals(expectedResult, result);
+		//_test.pass("Multiplication is working as expected , result is :" + result);
+	}
+
 }
