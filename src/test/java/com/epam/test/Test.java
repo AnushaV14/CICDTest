d44e5e00f2fda5f7856648f302bf99d6aec27b8f
Anusha Vujjini <anusha_vujjini@epam.com>
2024-06-17 14:32:18 +0530

    added test class

---
+++ src/test/java/com/epam/test/Test.java
@@ -0,0 +1,10 @@
+package com.epam.utilities;
+
+public class Test {
+	
+	public static void main(string args[])
+	{
+		TestUtility test = new TestUtility();
+		System.out.println("test successful :"+	test.generateRandomNumber());
+	}
+}
